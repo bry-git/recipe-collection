4bc64ec4fbb0de7c0d375fc613eeb5ecb0cfe702
bryson crandall <bryson.crandall@yahoo.com>
2021-07-29 14:24:58 -0700

    added recipe form functionality, testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
 
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isAddRecipeFormDisplayed: false,
+    list : []
+  }
+  
+  toggleAddRecipeForm = () => {
+    this.setState({isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed})
+  }
+
+  handleRecipeNameChange = (event) => {
+    const value = event.target.value;
+  
+    this.setState({newRecipeName: value});
+  }
+
+  handleRecipeInstructionsChange = (event) => {
+    const value = event.target.value;
+    this.setState({newRecipeInstructions: value})
+  }
+
+  submitRecipe = (event) => {
+    event.preventDefault()
+    this.setState({recipes: [
+        {
+          name: this.state.newRecipeName,
+          instructions :this.state.newRecipeInstructions
+        }
+      ]
+    })
+  }
+
+  render(){
+
+    const addNewRecipeForm = (
+      <form id="recipe-form" onSubmit={this.submitRecipe}>
+        <label htmlFor="newRecipeName">Recipe name: </label>
+        <input type="text" name="newRecipeName" id="newRecipeName" onChange={this.handleRecipeNameChange} value={this.state.newRecipeName} />
+        <label htmlFor="newRecipeInstructions">Instructions:</label>
+        <textarea name="newRecipeInstructions"
+          id="newRecipeInstructions"
+          placeholder="write recipe instructions here..." />
+        <input type="submit" />
+      </form>
+    )
+
+    return (
+      <div className="App">
+        <h1 className="App-header">My Recipes</h1>
+        {
+          this.state.isAddRecipeFormDisplayed
+           ? addNewRecipeForm
+          : <button id="add-recipe" onClick={this.toggleAddRecipeForm}> Add Recipe</button>
+        }
+        <p>There are no recipes to list.</p>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,8 +1,39 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('recipe collection', () => {
+
+  test('Add recipe button toggles visibility of a form on the page ', () => {
+
+    render(<App />);
+    // `queryBy...` methods will return null if the element is not found:
+    const recipeForm = screen.queryByText("Instructions:");
+  
+    // `getBy...` methods will "throw" an error if the element is not found:
+    // const recipeForm = screen.getByText("Instructions:");
+  
+    expect(recipeForm).toBeNull();
+    userEvent.click(screen.getByText("Add Recipe"));
+  
+    expect(screen.getByLabelText("Instructions:")).toBeInTheDocument();
+  });
+
+ 
+  it('renders the Recipe list', () => {
+
+  })
+
+  it('has an add recipe function that adds recipe to list', () => {
+
+  })
+
+  it('an added recipe can have details', () => {
+
+  })
+
+})
+
