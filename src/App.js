353b7baa16ddd5f35f6ad89d4701707b5b1539ed
Ty Jereza <nexusspark.jerezt@gmail.com>
2021-07-30 08:54:19 -0700

    Tests Passed

--- src/App.js
+++ src/App.js
@@ -6,64 +6,70 @@ import React from 'react'
 class App extends React.Component {
   state = {
     isAddRecipeFormDisplayed: false,
-    list: []
+    recipes : [],
+    newRecipeName: "",
+    newRecipeInstructions: ""
   }
-
+  
   toggleAddRecipeForm = () => {
-    this.setState({ isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed })
+    this.setState({isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed})
   }
 
-  handleRecipeNameChange = (event) => {
-    const value = event.target.value;
-    this.setState({ newRecipeName: value });
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    this.setState({[name]: target.value});
   }
-
-  handleRecipeInstructionsChange = (event) => {
-    const value = event.target.value;
-    this.setState({ newRecipeInstructions: value })
-  }
+  
 
   submitRecipe = (event) => {
-    console.log(this.state)
+    //const submittedRecipe = { name: recipeName, instructions: recipeInstructions }
     event.preventDefault()
-    this.setState({
-      recipes: [
+    this.setState({recipes: [
         {
           name: this.state.newRecipeName,
-          instructions: this.state.newRecipeInstructions
+          instructions :this.state.newRecipeInstructions
         }
       ]
     })
   }
 
-  render() {
+  render(){
+  const addNewRecipeForm = (
+    <form id="recipe-form" onSubmit={this.submitRecipe}>
+      <label htmlFor="newRecipeName">Recipe name: </label>
+      <input type="text"
+        name="newRecipeName"
+        id="newRecipeName"
+        onChange={this.handleChange}
+        value={this.state.newRecipeName} />
+      <label htmlFor="newRecipeInstructions">Instructions:</label>
+      <textarea name="newRecipeInstructions"
+        id="newRecipeInstructions"
+        placeholder="write recipe instructions here..."
+        onChange={this.handleChange}
+        value={this.state.newRecipeInstructions} />
+      <input type="submit" />
+    </form>
+  )
 
-    const addNewRecipeForm = (
-      <form id="recipe-form" onSubmit={this.submitRecipe}>
-        <label htmlFor="newRecipeName">Recipe name: </label>
-        <input type="text" name="newRecipeName" id="newRecipeName" onChange={this.handleRecipeNameChange} value={this.state.newRecipeName} />
-        <label htmlFor="newRecipeInstructions">Instructions:</label>
-        <input name="newRecipeInstructions"
-          id="newRecipeInstructions"
-          onChange={this.handleRecipeInstructionsChange}
-          placeholder="write recipe instructions here..."
-          value={this.state.newRecipeInstructions}
-        />
-        <input type="submit" />
-      </form>
-    )
-
-    return (
-      <div className="App">
-        <h1 className="App-header">My Recipes</h1>
-        {
-          this.state.isAddRecipeFormDisplayed
-            ? addNewRecipeForm
-            : <button id="add-recipe" onClick={this.toggleAddRecipeForm}> Add Recipe</button>
-        }
+  return (
+    <div className="App">
+      <h1 className="App-header">My Recipes</h1>
+      {
+        this.state.isAddRecipeFormDisplayed
+          ? addNewRecipeForm
+        : <button id="add-recipe" onClick={this.toggleAddRecipeForm}> Add Recipe</button>
+      }
+      {
+        this.state.recipes.length > 0 ?
+        <ul>
+          <li>{ this.state.recipes[0].name }</li>
+        </ul> :
         <p>There are no recipes to list.</p>
-      </div>
-    )
+      }
+    </div>
+  )
   }
 }
 
--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -4,8 +4,23 @@ import App from '../App';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
+const setup = () => {
+  const app = render(<App />);
 
-describe('recipe collection', () => {
+  userEvent.click(app.getByText('Add Recipe'));
+
+  // Add the submit button to your setup method:
+  const submitButton = app.getByRole('button')
+  const instructionsInput = app.getByLabelText('Instructions:')
+  const nameInput = app.getByLabelText('Recipe name:')
+
+  return {
+    instructionsInput,
+    nameInput,
+    submitButton
+  }
+}
+
 
   test('Add recipe button toggles visibility of a form on the page ', () => {
 
@@ -32,6 +47,27 @@ describe('recipe collection', () => {
     expect(screen.getByLabelText('Recipe name:').value).toEqual(recipeName);
   })
 
+  test('typing in the recipe instructions makes the instructions appear in the form', async () => {
+    const {instructionsInput} = setup();
+  
+    const recipeInstructions = "kinda hard to write instructions without knowing what I'm cooking"
+  
+    await userEvent.type(instructionsInput, recipeInstructions)
+    expect(instructionsInput.value).toEqual(recipeInstructions);
+  })
+
+  test('recipe name from state appears in an unordered list', async () => {
+    const {instructionsInput, nameInput, submitButton} = setup();
+    const recipeName = "Lean Pockets"
+    const recipeInstructions = "place in toaster oven on 350 for 45 minutes"
+  
+    await userEvent.type(instructionsInput, recipeInstructions)
+    await userEvent.type(nameInput, recipeName)
+    userEvent.click(submitButton);
+  
+    expect(screen.getByRole('listitem')).toBeInTheDocument();
+    expect(screen.getByText(recipeName)).toBeInTheDocument();
+  })
  
   it('renders the Recipe list', () => {
 
@@ -45,5 +81,4 @@ describe('recipe collection', () => {
 
   })
 
-})
 
